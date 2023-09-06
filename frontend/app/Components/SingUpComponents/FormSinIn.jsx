7d202d5e38f991fffec56fbe18eec4e6be7de832
FabricioReyna <fabricioreynacordes@gmail.com>
2023-09-06 18:30:49 -0300

    TransferResponsive and SingUp.V1.2

--- frontend/app/Components/SingUpComponents/FormSinIn.jsx
+++ frontend/app/Components/SingUpComponents/FormSinIn.jsx
@@ -3,16 +3,15 @@ import React from 'react'
 import PageTitle from "../FormComp/Title";
 import SubTitle from "../FormComp/SubTitle";
 import { useState } from "react";
-import {  ErrorMessage, Field, Form, Formik } from "formik";
+import {   Form, Formik } from "formik";
 import handleSignup from "./data";
 import * as Yup from "yup";
 import FirstView from './cards/FirstView';
 import SecondView from './cards/SecondView';
-import Button from '../Button';
-import FormInput from '../FormComp/Input';
 
 function FormSinIn() {
     const [ViewRegister, setViewRegister] = useState(true)
+    const apiUrl = 'http://localhost:3000/api/user/create';
 
     const signupSchema = () =>
       Yup.object().shape({
@@ -30,9 +29,10 @@ function FormSinIn() {
   
       });
   
-    const handleSubmit = async (values, { setSubmitting, setFieldError }) => {
-      await handleSignup(values, setSubmitting, setFieldError);
-    };
+  const handleSubmit = async (values, { setSubmitting, setFieldError }) => {
+    
+    await handleSignup(values, apiUrl, setSubmitting, setFieldError);
+  };
     const handleButtonClick = () =>{
       setViewRegister(false)
     }
@@ -50,90 +50,13 @@ function FormSinIn() {
       <PageTitle title={"Registrate"} />
   { ViewRegister ? ( 
 
+<FirstView handleButtonClick={handleButtonClick}/>
 
-
-<div className="bg-Grises/50 p-7 rounded-lg w-login shadow-lg mt-16 duration-300 transition-all">
-<FormInput labelHtmlFor="text" label="Nombre completo" />
-<Field
-  type="text"
-  name="name"
-  placeholder="Ingresar nombre"
-  className="w-full h-12 p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 mt-2  text-lg placeholder-Grises/350 mb-4"
-/>
-
-<ErrorMessage
-  name="name"
-  component="p"
-  className="text-red-500 text-sm mb-4"
-/>
-
-<FormInput labelHtmlFor="text" label="Nombre de usuario" />
-<Field
-  type="text"
-  name="lastName"
-  placeholder="@username"
-  className="w-full h-12 p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 mt-2 text-lg placeholder-Grises/350 mb-2"
-/>
-
-<ErrorMessage
-  name="lastName"
-  component="p"
-  className="text-red-500 text-sm mb-4"
-/>
-
-
-
-<div className="flex mt-8">
-  <Button  text="continuar" variant="filled" onClick={handleButtonClick} />
-</div>
-</div>
     
     
  ) : (
   
-  <div className="bg-Grises/50 p-7 rounded-lg w-login shadow-lg mt-16 transition-all duration-300">
-  <FormInput labelHtmlFor="password" label="Contraseña" />
- <Field
-   type="password"
-   name="password"
-   placeholder="Ingresar contraseña"
-   className="w-full h-12 p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 mt-2 text-lg placeholder-Grises/350 mb-2"
- />
-
- <ErrorMessage
-   name="password"
-   component="p"
-   className="text-red-500 text-sm mb-4"
- />
-
- <FormInput labelHtmlFor="email" label="Correo electrónico" />
- <Field
-   type="email"
-   name="email"
-   placeholder="Ingresar correo electrónico"
-   className="w-full h-12 p-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500 mt-2 text-lg placeholder-Grises/350 mb-2"
- />
-
- <ErrorMessage
-   name="email"
-   component="p"
-   className="text-red-500 text-sm mb-4"
- />
-
-
-   
-   <div className="flex mt-8 justify-evenly items-center w-full  ">
-   <div className="w-1/2 mr-2">
-
-   <Button text="volver"  variant="filled"  onClick={handleButtonReturnClick}  />
-
-   </div>
-   <div className="w-1/2 ml-2">
-   <Button type="submit" text="registrar" variant="filled"   />
-
-   </div>
- </div>
- </div>
+ <SecondView handleButtonReturnClick={handleButtonReturnClick}/>
   
 
       
