b6b4aa6e643af89cd71cf7e676ceea1a46087361
emilio rivas <emiliorivasruiz@gmail.com>
2023-09-08 22:39:18 -0500

    crear obtener y eliminar contactos

--- backend/src/services/associated_acounts.services.js
+++ backend/src/services/associated_acounts.services.js
@@ -1,68 +1,69 @@
-const AppError = require('../helpers/AppError')
-const db = require('../models/index')
-const AccountServices = require('./account.services')
-const UserServices = require('./user.services')
+const AppError = require('../helpers/AppError');
+const db = require('../models/index');
+const AccountServices = require('./account.services');
+const UserServices = require('./user.services');
 
 class AssociatedAccServices {
-    accountService = new AccountServices()
+    accountService = new AccountServices();
 
     async getAllAssociated({ id }) {
-        const userService = new UserServices()
+        // const userService = new UserServices();
         try {
-            const user = await userService.findOneUser({
-                attributes: { id: id },
-            })
-            if (!user) {
-                throw next(new AppError('User does not exist', 400));
-            }
-            const userId = user.id
-            const userAssociated = await db.Associated_Accounts.findOne({
+            // const user = await userService.findOneUser({
+            //     attributes: { id: id },
+            // });
+            // if (!user) {
+            //     throw next(new AppError('User does not exist', 400));
+            // }
+            // const userId = user.id;
+            const userAssociated = await db.Associated_Accounts.findAll({
                 where: { userId: id },
-            })
-            return userAssociated
+            });
+            return userAssociated;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async createAssociated({ id, body, next }) {
-        const userService = new UserServices()
+    async createAssociated({ sessionUser, body, next }) {
         try {
-            const user = await userService.findOneUser({
-                attributes: { id: id },
-            })
-            if (!user) {
-                throw next(new AppError('User does not exist', 400));
-            }
-            const userId = user.id
+            const { contact_account_number, contactNickName } = body;
+            const { id } = sessionUser;
+
+            const contactAccount = await this.accountService.findOneAccount({
+                attributes: { account_number: contact_account_number },
+                next,
+            });
+
             const createAssociated = await db.Associated_Accounts.create({
-                userId
-            })
-            return createAssociated
+                userId: id,
+                contactId: contactAccount.userId,
+                contact_account_number,
+                contactNickName,
+            });
+            return createAssociated;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 
-    async deleteAssociated({ id }) {
-        const userService = new UserServices()
+    async deleteAssociated({ id, contactId }) {
+        // const userService = new UserServices();
         try {
-            const user = await userService.findOneUser({
-                attributes: { id: id },
-            })
-            if (!user.id) {
-                throw next(new AppError('User does not exist', 400));
-            }
+            // const user = await userService.findOneUser({
+            //     attributes: { id: id },
+            // });
+            // if (!user.id) {
+            //     throw next(new AppError('User does not exist', 400));
+            // }
             const deletedAssociated = await db.Associated_Accounts.destroy({
-                where: { userId: id }
-            })
-            return deletedAssociated
+                where: { userId: id, contactId },
+            });
+            return deletedAssociated;
         } catch (error) {
-            throw new Error(error)
+            throw new Error(error);
         }
     }
 }
 
-module.exports = AssociatedAccServices
-
-
+module.exports = AssociatedAccServices;
