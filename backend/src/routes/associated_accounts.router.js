96cad63e1e56bc354927fb6b6e2da9d4c937b1c1
Matias Paredes <tutecapo1985@gmail.com>
2023-09-08 21:19:36 -0300

    refactoring feature

--- backend/src/routes/associated_accounts.router.js
+++ backend/src/routes/associated_accounts.router.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const {createAssociated,deleteAssociated,getAllAssociated} = require('../controllers/associated_account.controller')
-const { protectRoute, verifyAccountOwner } = require('../middlewares/auth.middleware')
 
 const router = express.Router()
 
