d8c25f136de781c49e46d3f1592af1fb411b58f8
emilio rivas <emiliorivasruiz@gmail.com>
2023-09-12 20:40:26 -0500

    segundo intento  de merge

--- backend/src/controllers/account.controller.js
+++ backend/src/controllers/account.controller.js
@@ -1,30 +1,10 @@
 const catchAsync = require('../helpers/catchAsync');
-const emailConfirmation = require('../middlewares/emailConfirmation');
-const generateConfirmLink = require('../utils/generateConfirmLink');
 const AccountServices = require('../services/account.services');
 const accountServices = new AccountServices();
 
-exports.createAccount = catchAsync(async (req, res) => {
+exports.createAccount = catchAsync(async (req, res, next) => {
     const userId = req.params.id;
-
-    const userHasAccount = await accountServices.hasAccount(userId);
-    if (userHasAccount) {
-        return res.status(400).json({
-            status: 'fail',
-            message: 'User already has an account.',
-        });
-    }
-
-    const createdAccount = await accountServices.createAccount(userId);
-
-    const confirmLink = generateConfirmLink(createdAccount.id);
-
-    const { sessionUser } = req;
-    const confirmation = await emailConfirmation(
-        sessionUser.email,
-        confirmLink,
-    );
-
+    const createdAccount = await accountServices.createAccount(userId, next);
     res.status(200).json({
         status: 'success',
         message: 'Account created and saved to database',
@@ -39,32 +19,9 @@ exports.rechargeCard = catchAsync(async (req, res, next) => {
 
     res.status(200).json({
         status: 'success',
-        message: 'You have recharged your account',
+        message: 'haz recargado tu cuenta',
         charge,
     });
-});
-
-exports.confirmAccount = catchAsync(async (req, res) => {
-    const token = req.params.token;
-    const result = await accountServices.confirmAccount(token, 'confirmed');
-
-    res.status(200).json({
-        status: 'success',
-        message: 'has obtenido tu cuenta',
-        account,
-    });
-    if (result === 'confirmed') {
-        return res.status(200).json({
-            status: 'success',
-            message: 'Account successfully confirmed',
-            account: result,
-        });
-    } else {
-        return res.status(400).json({
-            status: 'error',
-            message: 'Error confirming account. Please contact support.',
-        });
-    }
 });
 
 exports.getUserAccount = catchAsync(async (req, res, next) => {
